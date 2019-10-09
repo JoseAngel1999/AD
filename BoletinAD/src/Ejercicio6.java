2d805bcee023c1923697e89a8928917afce7e54c
JoseAngel1999 <49056860+JoseAngel1999@users.noreply.github.com>
2019-10-09 13:04:27 +0200

    Update Ejercicio6.java

--- BoletinAD/src/Ejercicio6.java
+++ BoletinAD/src/Ejercicio6.java
@@ -1,16 +1,26 @@
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Ejercicio6 {
-	
-	public static void directorioPasado(File fichero){
-		
-		File nombreFile = new File("C:\\Users\\josea\\Documents\\2DAM");
-		
-		}
+
+	public static void directorioPasado(){
+		Date myDate = new Date();
+
+		try { 
+
+			File f = new File("C:\\Users\\josea\\Documents\\2DAM\\"+ new SimpleDateFormat("dd-MM-yyyy").format(myDate).toString()); 
+			f.mkdir();
+		} 
+		catch (Exception e) { 
+			System.err.println(e); 
+		} 
+	} 
+
 
 	public static void main(String[] args) {
-		
-		new directorioPasado();
+
+		directorioPasado();
 
 	}
 }
